86bf8391ffaac20156c53f84f6de5141d24a6985
Young Kim <young_github@chillin.dev>
2020-05-04 02:41:03 -0700

    fixed css background image error

--- public/src/assets/js/mobile.js
+++ public/src/assets/js/mobile.js
@@ -3,12 +3,12 @@ function openMenu() {
   document.querySelector(".mobile-nav").style.height = "13rem";
   document.querySelector(".mobile-menu-open-btn").style.display = "none";
   document.querySelector(".mobile-menu-close-btn").style.display = "inline";
-  document.querySelector(".mobile-nav-links").style.height = "100%";
+  document.querySelector(".mobile-nav-links").style.display = "block";
 }
 
 function closeMenu() {
   document.querySelector(".mobile-nav").style.height = "0";
   document.querySelector(".mobile-menu-open-btn").style.display = "inline";
   document.querySelector(".mobile-menu-close-btn").style.display = "none";
-  document.querySelector(".mobile-nav-links").style.height = "0";
+  document.querySelector(".mobile-nav-links").style.display = "none";
 }
