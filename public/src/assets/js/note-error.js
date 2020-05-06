8889c661b6933abbda8428811d10c8624f754a17
Young Kim <young_github@chillin.dev>
2020-05-06 11:23:23 -0700

    fixed not to have error message of non-notification page, and changed color of notification

---
+++ public/src/assets/js/note-error.js
@@ -0,0 +1,10 @@
+setTimeout(function () {
+    document.querySelector('.main-notification-error').style.height='2rem';
+    document.querySelector('.main-notification-error span').style.opacity='1';
+}, 500);
+
+setTimeout(function () {
+    document.querySelector('.main-notification-error span').style.opacity='0';
+    document.querySelector('.main-notification-error').style.height='0';
+}, 3000);
+
