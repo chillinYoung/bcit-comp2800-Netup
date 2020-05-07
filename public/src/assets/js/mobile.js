fd8f5d99c4ddaeded5493bac2f91363d3bf686cb
Young Kim <young_github@chillin.dev>
2020-05-06 20:05:12 -0700

    fixed mobile nav list

--- public/src/assets/js/mobile.js
+++ public/src/assets/js/mobile.js
@@ -1,7 +1,7 @@
 function openMenu() {
-  // document.querySelector(".mobile-nav").style.height = "100%";
-  document.querySelector(".mobile-nav").style.height = "13rem";
-  document.querySelector(".mobile-nav-links").style.height = "13rem";
+  document.querySelector(".mobile-nav").style.height = "calc(100vh - 4.7rem)";
+  // document.querySelector(".mobile-nav").style.height = "100vh";
+  document.querySelector(".mobile-nav-links").style.height = "100%";
   document.querySelector(".mobile-menu-open-btn").style.display = "none";
   document.querySelector(".mobile-menu-close-btn").style.display = "inline";
 }
@@ -12,3 +12,11 @@ function closeMenu() {
   document.querySelector(".mobile-menu-open-btn").style.display = "inline";
   document.querySelector(".mobile-menu-close-btn").style.display = "none";
 }
+
+function resizeMenu() {
+  if (window.matchMedia("(min-width: 640px)").matches) {
+    closeMenu();
+  }
+}
+
+window.onresize = function() {resizeMenu()};
