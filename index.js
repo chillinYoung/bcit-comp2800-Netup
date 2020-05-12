9925259c86b84cd8e5aa0ec6c9c84973d25a1405
jamesreinhardt222 <jreinhardt2@mybcit.ca>
2020-05-12 15:01:58 -0700

    Refactored contact-form code

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const ejsLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require('dotenv').config();
-const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
 const schema = require("./db/mongooseSchema");
-const nodemailer = require('nodemailer');
-const path = require("path")
+
 
 
 // instantiate express app
@@ -235,53 +231,7 @@ server.get('/contact', (req, res) => {
   res.render('pages/contact');
 });
 
-server.post('/send', (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>  
-      <li>Name: ${req.body.name}</li>
-      <li>Company: ${req.body.company}</li>
-      <li>Email: ${req.body.email}</li>
-      <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</p>
-  `;
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: 'netupTestEmail@gmail.com', // generated ethereal user
-        pass: 'Netup123@'  // generated ethereal password
-    },
-    tls:{
-      rejectUnauthorized:false
-    }
-  });
-
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: `"Nodemailer Contact" ${req.body.email}`, // sender address
-      to: 'netupTestEmail@gmail.com', // list of receivers
-      subject: 'User Contact Request', // Subject line
-      text: 'Feedback sent!', // plain text body
-      html: output // html body
-  };
-
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render('pages/contact', {msg:'Email has been sent'});
-  });
-  });
+server.post('/send', mongooseFunctions.contactForm);
 
 
 // 404 page route (Please keep at the very bottom)
