ed7d2df1767e09b0afead058572a0ae2fea267a2
jamesreinhardt222 <jreinhardt2@mybcit.ca>
2020-05-12 13:24:48 -0700

    Finished contact form

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 const schema = require("./db/mongooseSchema");
+const nodemailer = require('nodemailer');
+const path = require("path")
 
 
 // instantiate express app
@@ -227,6 +229,59 @@ server.post("/login", (req, res, next) => {
 });
 
 server.post("/create", userController.createEvent);
+
+// contact form will be moved to it's own file later.
+server.get('/contact', (req, res) => {
+  res.render('pages/contact');
+});
+
+server.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'netupTestEmail@gmail.com', // generated ethereal user
+        pass: 'Netup123@'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: `"Nodemailer Contact" ${req.body.email}`, // sender address
+      to: 'netupTestEmail@gmail.com', // list of receivers
+      subject: 'User Contact Request', // Subject line
+      text: 'Feedback sent!', // plain text body
+      html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('pages/contact', {msg:'Email has been sent'});
+  });
+  });
 
 
 // 404 page route (Please keep at the very bottom)
