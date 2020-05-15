4e02f53baaacb3fd11d74b345c95fc58ba24b037
Young Kim <young_github@chillin.dev>
2020-05-15 16:13:49 -0700

    added join button change on details page

--- public/src/assets/js/event_controls.js
+++ public/src/assets/js/event_controls.js
@@ -5,3 +5,12 @@ function joinToJoined(eventIdAsId) {
   document.getElementById(eventIdAsId).removeAttribute("onclick");
   document.getElementById(eventIdAsId).setAttribute("class", "video-post-joined");
 }
+
+function joinToJoinedDetail(eventIdAsId) {
+  document.getElementById(eventIdAsId).innerHTML = "Joined";
+  document.getElementById(eventIdAsId).removeAttribute("formmethod");
+  document.getElementById(eventIdAsId).removeAttribute("formaction");
+  document.getElementById(eventIdAsId).removeAttribute("onclick");
+  document.getElementById(eventIdAsId).setAttribute("class", "details-joined-btn");
+}
+
