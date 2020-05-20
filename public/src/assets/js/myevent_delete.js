e4aa034d1c15063ae61eb99f50b226283bdbaa9b
Young Kim <young_github@chillin.dev>
2020-05-19 23:57:19 -0700

    fixed transition bug for event delete warning

--- public/src/assets/js/myevent_delete.js
+++ public/src/assets/js/myevent_delete.js
@@ -1,8 +1,11 @@
 function deleteConfirm(id) {
   closeAllConfirm();
   document.querySelector(".warn-" + id).style.width = "100%";
-  document.querySelector(".warn-" + id + " span").style.display = "block";
-  document.querySelector(".warn-" + id + " div").style.display = "flex";
+  setTimeout(function () {
+    document.querySelector(".warn-" + id + " span").style.display = "block";
+    document.querySelector(".warn-" + id + " div").style.display = "flex";
+    document.querySelector(".warn-" + id + " span").style.opacity = "1";
+  }, 300);
 }
 
 function closeAllConfirm() {
@@ -14,6 +17,9 @@ function closeAllConfirm() {
   });
   document.querySelectorAll(".myevents-warn div").forEach(elem => {
     elem.style.display = "none";
+  });
+  document.querySelectorAll(".myevents-warn span").forEach(elem => {
+    elem.style.opacity = "0";
   });
 }
 
