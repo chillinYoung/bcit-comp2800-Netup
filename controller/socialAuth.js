6e404878911643dc742d514bff129c5ab9c891b7
jamesreinhardt222 <jreinhardt2@mybcit.ca>
2020-05-21 17:09:44 -0700

    Commented social-media login file..

--- controller/socialAuth.js
+++ controller/socialAuth.js
@@ -2,30 +2,32 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github').Strategy;
 
+// Module for social media login logic.
 
 module.exports = {
+    // Configure passport to use the google sign-in api wit hthe correct credentials.
     googleSetup: passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
         callbackURL: "https://dtc10-netup.herokuapp.com/auth/google/secrets",
         userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
       },
+            // Logic for bringing up the google sign-in interface to the screen.
       function(accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-      
         // Creates a user in the database for the logged-in gmail account.
         User.findOrCreate({ googleId: profile.id, name: profile.displayName }, function (err, user) {
           return cb(err, user);
         });
       }
       )),
+      // Callback function for responding to a user signing in.
       getGoogleSigninRoute: passport.authenticate('google', { scope: ["profile"] }),
       googleCallback:  passport.authenticate('google', { failureRedirect: "/login" }),
       function(req, res) {
-        console.log(req.user)
         // Successful authentication, redirect to myEvents.
         res.redirect("/myEvents");
       },
+      // Prepare passport to use github sign-in api with the correct credentials.
       githubSetup:  passport.use(new GitHubStrategy({
         clientID: process.env.GITHUBID,
         clientSecret: process.env.GITHUBSECRET,
@@ -38,7 +40,9 @@ module.exports = {
         });
       }
       )),
+      //  Set up Github-login.
       authenticateGithub: passport.authenticate('github'),
+    //   Callback function for after user signs in with Github.
       githubCallback: passport.authenticate('github', { successRedirect : '/myEvents', failureRedirect: '/login' }),
       function(req, res) {
         // Successful authentication, redirect home.
