4776fe87739485b02af14832d1f6be483b3ba945
jamesreinhardt222 <jreinhardt2@mybcit.ca>
2020-05-21 21:55:10 -0700

    Fixed another social loggin bug

--- db/socialAuth.js
+++ db/socialAuth.js
@@ -13,8 +13,8 @@ module.exports = {
     googleSetup: passport.use(new GoogleStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "https://dtc10-netup.herokuapp.com/auth/google/secrets",
-        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+        callbackURL: "http://dtc10-netup.herokuapp.com/auth/google/secrets",
+        userProfileURL: "http://www.googleapis.com/oauth2/v3/userinfo"
       },
             // Logic for bringing up the google sign-in interface to the screen.
       function(accessToken, refreshToken, profile, cb) {
